0ad3e4f7abfd78bec96e6c18933ecfbfca3754d9
kerroggu <49674292+kerroggu@users.noreply.github.com>
2019-11-16 18:42:37 +0900

    1st creation
    Dijkstra: gives minimum distances from a start point and list of previous vertex in the minimum path
    tested by ABC 035 D
    https://atcoder.jp/contests/abc035/tasks/abc035_d

---
+++ Dijkstra.py
@@ -0,0 +1,21 @@
+from heapq import heappush, heappop,heapify
+
+def dijkstra(edge,st):
+    # edge=[[(v_to,dist_to_v),...],[],...]
+    # initialize: def: d=dist(st,i), prev=[previous vertex in minimum path], q[]
+    n=len(edge)
+    d=[(0 if st==i else inf,i) for i in range(n)]
+    prev=[0]*n
+    q=[i for i in d]
+    heapify(q)
+    
+    # calc
+    while q:
+        dist,cur=heappop(q)
+        for dst,dist in edge[cur]:
+            alt=d[cur][0]+dist
+            if alt<d[dst][0]:
+                d[dst]=(alt,dst)
+                prev[dst]=cur
+                heappush(q,(alt,dst))
+    return d,prev
