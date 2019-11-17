cdfbd1dc0d5ba02b3a07f999283e9b4217938a35
kerroggu <49674292+kerroggu@users.noreply.github.com>
2019-11-17 13:54:48 +0900

    fixed initilization
    tested by ABC 145 D
    https://atcoder.jp/contests/abc145/tasks/abc145_d

--- Comb.py
+++ Comb.py
@@ -4,7 +4,8 @@ class Comb:
         self.inv=[1]*(n+1)
         self.fac[0]=1
         self.fact(n)
-        for i in range(2,n+1):
+        for i in range(1,n+1):
+            self.fac[i]=i*self.fac[i-1]%mo
             self.inv[n]*=i
             self.inv[n]%=mo
         self.inv[n]=pow(self.inv[n],mo-2,mo)
@@ -13,11 +14,8 @@ class Comb:
         return
     
     def fact(self,n):
-        if self.fac[n]!=0:
-            return self.fac[n]
-        self.fac[n]=n*self.fact(n-1)%mo
         return self.fac[n]
-
+        
     def invf(self,n):
         return self.inv[n]
 
