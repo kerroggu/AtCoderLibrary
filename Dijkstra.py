3850f6d3462c022f6077a2ef151c1dae756566f7
keroru <49674292+kerroggu@users.noreply.github.com>
2020-04-27 23:41:09 +0900

    add pruning by determined node
    tested by ARC 064 E
    https://atcoder.jp/contests/arc064/tasks/arc064_c

--- Dijkstra.py
+++ Dijkstra.py
@@ -4,14 +4,17 @@ def dijkstra(edge,st):
     # edge=[[(v_to,dist_to_v),...],[],...]
     # initialize: def: d=dist(st,i), prev=[previous vertex in minimum path], q[]
     n=len(edge)
-    d=[(0 if st==i else inf) for i in range(n)]
+    d=[(0 if st==i else 1<<33) for i in range(n)]
     prev=[0]*n
     q=[(j,i) for i,j in enumerate(d)]
     heapify(q)
-    
+    remain=set(range(n))
     # calc
     while q:
         dist,cur=heappop(q)
+        if cur not in remain:
+            continue
+        remain.remove(cur)
         for dst,dist in edge[cur]:
             alt=d[cur]+dist
             if alt<d[dst]:
