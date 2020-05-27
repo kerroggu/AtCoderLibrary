b7f9cad498bb217726025fa4061de04e17cb14dd
keroru <49674292+kerroggu@users.noreply.github.com>
2020-05-27 22:52:26 +0900

    Updated
    Base code
    https://juppy.hatenablog.com/entry/2019/02/16/%E8%9F%BB%E6%9C%AC_python_%E6%9C%80%E5%B0%8F%E5%85%A8%E5%9F%9F%E6%9C%A8%E5%95%8F%E9%A1%8C%EF%BC%91%EF%BC%88heap%E3%82%92%E7%94%A8%E3%81%84%E3%81%9F%E3%83%97%E3%83%AA%E3%83%A0%E6%B3%95%EF%BC%89_%E7%AB%B6

--- PrimHeap.py
+++ PrimHeap.py
@@ -1,18 +1,23 @@
-def prim_heap(edge):
-    v=[0]*n
-    q=g[0][:]
-    heapify(q)
-    v[0]=1
-    rt=[]
-    pre=0
-    while q:
-        d,(cur,num)=heappop(q)
-        if v[cur]==1:
+## Tested by PAST_001 L
+## https://atcoder.jp/contests/past201912-open/tasks/past201912_l
+
+from heapq import heappush,heappop,heapify
+
+def prim(edge):
+    n=len(edge)
+    used=[True]*n #True:不使用
+    edgelist=[]
+    for node,dist in edge[0]:
+        heappush(edgelist,(dist,node))
+    used[0]=False
+    res=0
+    while edgelist:
+        dist,node=heappop(edgelist)
+        if not used[node]:
             continue
-        v[cur]=1
-        rt+=[num+1]
-        pre=cur
-        for nd,(nb,num) in g[cur]:
-            if v[nb]==0:
-                heappush(q,(nd,(nb,num)))
-    return rt
+        used[node]=False
+        for nx,nx_dist in edge[node]:
+            if used[nx]:
+                heappush(edgelist,(nx_dist,nx))
+        res+=dist
+    return res
