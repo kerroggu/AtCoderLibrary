0abe3a4d84bf972262b3f885dbeec6059637f6d6
keroru <49674292+kerroggu@users.noreply.github.com>
2020-05-29 13:18:32 +0900

    Added left/right_bound function
    Doubling search for bound

--- SparseTable.py
+++ SparseTable.py
@@ -1,3 +1,6 @@
+## Tested by EduFor 88 D
+## https://codeforces.com/contest/1359/problem/D
+
 class SparseTable: # O(N log N) for init, O(1) for query(l,r)
     def __init__(self,ls,init_func=min,init_idl=float('inf')):
         self.func=init_func
@@ -10,7 +13,7 @@ class SparseTable: # O(N log N) for init, O(1) for query(l,r)
         
         for i in range(2,self.size+1):
             self.lg[i]=self.lg[i>>1]+1
-
+ 
         for i in range(self.N0):
             tmp=[self.func(self.table[i][j],self.table[i][min(j+(1<<i),self.size-1)]) for j in range(self.size)]
             tmp_id=[self.index[i][j] if self.table[i][j]==self.func(self.table[i][j],self.table[i][min(j+(1<<i),self.size-1)]) else self.index[i][min(j+(1<<i),self.size-1)] for j in range(self.size)]
@@ -19,22 +22,40 @@ class SparseTable: # O(N log N) for init, O(1) for query(l,r)
     
     # return func of [l,r)
     def query(self,l,r):
+        if r>self.size:r=self.size
         #N=(r-l).bit_length()-1
         N=self.lg[r-l]
-        return self.func(self.table[N][l],self.table[N][r-(1<<N)])
+        return self.func(self.table[N][l],self.table[N][max(0,r-(1<<N))])
     
     # return index of which val[i] = func of v among [l,r)
     def query_id(self,l,r):
+        if r>self.size:r=self.size
         #N=(r-l).bit_length()-1
         N=self.lg[r-l]
-        a,b=self.index[N][l],self.index[N][r-(1<<N)]
-        if self.table[0][a]==self.func(self.table[N][l],self.table[N][r-(1<<N)]):
+        a,b=self.index[N][l],self.index[N][max(0,r-(1<<N))]
+        if self.table[0][a]==self.func(self.table[N][l],self.table[N][max(0,r-(1<<N))]):
             b=a
         return b
     
+    def right_bound(self,pos,x):
+        k=(self.size-pos).bit_length()
+        for j in range(k)[::-1]:
+            nx=pos+(1<<j)
+            if nx<n and self.query(pos,nx+1)<=x:
+                pos+=(1<<j)
+        return pos
+    
+    def left_bound(self,pos,x):
+        k=pos.bit_length()
+        for j in range(k)[::-1]:
+            nx=pos-(1<<j)
+            if 0<=nx and self.query(nx,pos+1)<=x:
+                pos-=(1<<j)
+        return pos
+
     def __str__(self):
         return str(self.table[0])
-        
+ 
     def print(self):
         for i in self.table:
             print(*i)
