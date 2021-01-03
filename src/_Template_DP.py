3b6331ced0fdf82678c800f2e6ff490a7f3bb717
keroru <49674292+kerroggu@users.noreply.github.com>
2021-01-03 16:59:40 +0900

    added bit subgroup DP

--- src/_Template_DP.py
+++ src/_Template_DP.py
@@ -21,3 +21,33 @@ for i in range(n):
 
 
 # DP     ------------------------------------------------------------------
+
+# Bit subgroup DP     ------------------------------------------------------------------
+# https://atcoder.jp/contests/abc187/tasks/abc187_f
+n,m=LI()
+g,_=GI(n,m)
+chk=[None]*(1<<n)
+
+def is_cg(x):
+    if chk[x]!=None:return chk[x]
+    s=[j for j in range(n) if (x>>j)&1]
+    N=len(s)
+    for i in range(N):
+        for j in range(i+1,N):
+            if (s[i],1)not in g[s[j]] or (s[j],1)not in g[s[i]]:
+                return False
+    chk[x]=True
+    return True
+
+N=1<<n
+dp=[n]*N
+
+for i in range(N):
+    if is_cg(i):dp[i]=1
+    j=i
+    while j>0:
+        dp[i]=min(dp[i],dp[j]+dp[i^j])  # split group i to subset i and i/j
+        j-=1
+        j&=i
+
+print(dp[-1])
