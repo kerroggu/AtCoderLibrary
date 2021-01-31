1ceeaa9bb072ffc7cfa0acf3e6064f7252840eb6
keroru <49674292+kerroggu@users.noreply.github.com>
2021-01-31 18:16:23 +0900

    Added TSP

--- src/_Template_DP.py
+++ src/_Template_DP.py
@@ -20,10 +20,9 @@ for i in range(n):
             dp[1][(j+x)%d][i+1]%=mo
 
 
-# DP     ------------------------------------------------------------------
-
 # Bit subgroup DP     ------------------------------------------------------------------
-# https://atcoder.jp/contests/abc187/tasks/abc187_f
+# https://atcoder.jp/contests/abc190/tasks/abc190_e
+# 
 n,m=LI()
 g,_=GI(n,m)
 chk=[None]*(1<<n)
@@ -33,21 +32,26 @@ def is_cg(x):
     s=[j for j in range(n) if (x>>j)&1]
     N=len(s)
     for i in range(N):
-        for j in range(i+1,N):
-            if (s[i],1)not in g[s[j]] or (s[j],1)not in g[s[i]]:
-                return False
-    chk[x]=True
-    return True
 
-N=1<<n
-dp=[n]*N
+# TSP by Bit DP     ------------------------------------------------------------------
+# https://atcoder.jp/contests/abc187/tasks/abc187_f
+# https://atcoder.jp/contests/past202005-open/tasks/past202005_m
+
+# k := 頂点数
+# d[u][v] := u-v間の距離
+# dp[i][S] := bit集合Sの頂点を訪れて頂点iに辿り着く場合の最小距離 (出発点が決まっている場合の初期化はdp[st][1<<st]=0)
+
+N=1<<k
+dp=[[inf]*N for i in range(k)]
+for i in range(k):
+    dp[i][1<<i]=0
 
 for i in range(N):
-    if is_cg(i):dp[i]=1
-    j=i
-    while j>0:
-        dp[i]=min(dp[i],dp[j]+dp[i^j])  # split group i to subset i and i/j
-        j-=1
-        j&=i
+    for u in range(k):
+        for v in range(k):
+            if (i>>u)&1&(i>>v)==0: # u,vがbit集合に含まれないケースは除外
+                continue
+            if d[u][v]!=None: # u->vの経路が使える場合、最短距離を更新
+                dp[v][i]=min(dp[v][i],dp[u][i^(1<<v)]+d[u][v])
 
-print(dp[-1])
+ans=min([dp[i][-1] for i in range(k)])
