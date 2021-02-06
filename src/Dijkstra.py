c70d74b93015035df7553f32dd28d248b5799fe4
keroru <49674292+kerroggu@users.noreply.github.com>
2021-02-06 23:37:17 +0900

    Modified initial items in queue and commented out root search

--- src/Dijkstra.py
+++ src/Dijkstra.py
@@ -1,15 +1,17 @@
+## Verified by ABC191F
+## https://atcoder.jp/contests/abc191/tasks/abc191_e
+
 from heapq import heappush, heappop,heapify
 
 def dijkstra(edge,st):
-    # edge=[[(v_to,dist_to_v),...],[],...]
-    # initialize: def: d=dist(st,i), prev=[previous vertex in minimum path], q[]
+    ## edge=[[(v_to,dist_to_v),...],[],...]
+    ## initialize: def: d=dist(st,i), prev=[previous vertex in minimum path], q[]
     n=len(edge)
     d=[(0 if st==i else inf) for i in range(n)]
-    prev=[0]*n
-    q=[(j,i) for i,j in enumerate(d)]
+#    prev=[0]*n
+    q=[(0,st)]
     heapify(q)
     v=[False]*n
-    # calc
     while q:
         dist,cur=heappop(q)
         if v[cur]:
@@ -19,10 +21,9 @@ def dijkstra(edge,st):
             alt=d[cur]+dist
             if alt<d[dst]:
                 d[dst]=alt
-                prev[dst]=cur
+#                prev[dst]=cur
                 heappush(q,(alt,dst))
-    return d,prev
-
+    return d#,prev
 
 def golf_dijkstra():
  from heapq import*;n,m,k,*t=map(int,open(0).read().split());P=[t[i*3:i*3+3]for i in range(m)];s=t[3*m:];g=[[]for _ in' '*-~n];q=[(0,i)for i in s];v=[1]*-~n
