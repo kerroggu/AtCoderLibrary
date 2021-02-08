6f1bbfb56860ea86f06020deea8aea63f9c3f92a
keroru <49674292+kerroggu@users.noreply.github.com>
2021-02-08 13:44:01 +0900

    Update _Template_Doubling.py

--- src/_Template_Doubling.py
+++ src/_Template_Doubling.py
@@ -23,4 +23,23 @@ for _ in range(q):
             a=par[p][a]
     
     print(t+1)
-    
+
+
+## https://atcoder.jp/contests/arc020/tasks/arc020_3
+ 
+def db_f(a,l,m):
+    x=a
+    rt=0
+    cd=1
+    D=pow(10,len(str(a)),m)
+    k=l.bit_length()
+    for i in range(k):
+        if (l>>i)&1:
+            rt+=x*cd
+            cd*=D
+            cd%=mo
+        x=x*D+x
+        x%=m
+        
+        D=D*D%m
+    return rt
