3ba5818753a948e60d8368b316184236ad798060
keroru <49674292+kerroggu@users.noreply.github.com>
2021-02-09 00:32:53 +0900

    Update _Template_Interacitve.py

--- src/_Template_Interacitve.py
+++ src/_Template_Interacitve.py
@@ -1,19 +1,29 @@
+## verified by DISCO2020-E
+## https://atcoder.jp/contests/ddcc2020-qual/tasks/ddcc2020_qual_e
 
+def solve(n,mode='Sub'):
+    if mode!='Sub': # set a test case in the following block
+        s='BRRRBB'
+        n=len(s)//2
 
-def judge(x):
-    an=a[x-1]
-    print(an)
-    return an
+    def judge(x):
+        if len(x)!=n:
+            return 'Error'
+        b=[s[i] for i in x]
+        an=['Red','Blue'][b.count('B')>b.count('R')]
+        return an
+    
+    c=[0]
+    def query(x):
+        c[0]+=1
+        if c[0]>210 or len(set(x))!=n or min(x)<0 or max(x)>=2*n: ## query condition check
+            0/0
 
-def query(x): # for submission
-    print('?',x)
-    sys.stdout.flush()
-    y=I()
-    return y
-
-def query(x): # for local test
-    print('?',x)
-    sys.stdout.flush()
-    #y=I()
-    y=judge(x)
-    return y
+        print('?',*[i+1 for i in x])
+        sys.stdout.flush()
+        
+        if mode=='Sub': # for Submission
+            y=input()
+        else: # for Test
+            y=judge(x)
+        return y[0]
