41e733b044e2b8bc90df1ea9883db27a6a2092cb
keroru <49674292+kerroggu@users.noreply.github.com>
2021-02-24 23:49:22 +0900

    Added dfs_tour()

--- src/Tree.py
+++ src/Tree.py
@@ -9,82 +9,6 @@
 # Tree(init=False); Tree.stdin(); => n; (a,b);*(n-1)
 # Tree(init=False); Tree.listin(ls,index=0); => (a,b);*(n-1)
 
-import sys,bisect,string,math,time,functools,random,fractions
-from heapq import heappush,heappop,heapify
-from collections import deque,defaultdict,Counter
-from itertools import permutations,combinations,groupby
-rep=range
-def Golf():n,*t=map(int,open(0).read().split())
-def I():return int(input())
-def S_():return input()
-def IS():return input().split()
-def LS():return [i for i in input().split()]
-def MI():return map(int,input().split())
-def LI():return [int(i) for i in input().split()]
-def LI_():return [int(i)-1 for i in input().split()]
-def NI(n):return [int(input()) for i in range(n)]
-def NI_(n):return [int(input())-1 for i in range(n)]
-def StoLI():return [ord(i)-97 for i in input()]
-def ItoS(n):return chr(n+97)
-def LtoS(ls):return ''.join([chr(i+97) for i in ls])
-def RA():return map(int,open(0).read().split())
-def GI(V,E,ls=None,Directed=False,index=1):
-    org_inp=[];g=[[] for i in range(V)]
-    FromStdin=True if ls==None else False
-    for i in range(E):
-        if FromStdin:
-            inp=LI()
-            org_inp.append(inp)
-        else:
-            inp=ls[i]
-        if len(inp)==2:
-            a,b=inp;c=1
-        else:
-            a,b,c=inp
-        if index==1:a-=1;b-=1
-        aa=(a,c);bb=(b,c);g[a].append(bb)
-        if not Directed:g[b].append(aa)
-    return g,org_inp
-def GGI(h,w,search=None,replacement_of_found='.',mp_def={'#':1,'.':0},boundary=1):
-    #h,w,g,sg=GGI(h,w,search=['S','G'],replacement_of_found='.',mp_def={'#':1,'.':0},boundary=1) # sample usage
-    mp=[boundary]*(w+2);found={}
-    for i in range(h):
-        s=input()
-        for char in search:
-            if char in s:
-                found[char]=((i+1)*(w+2)+s.index(char)+1)
-                mp_def[char]=mp_def[replacement_of_found]
-        mp+=[boundary]+[mp_def[j] for j in s]+[boundary]
-    mp+=[boundary]*(w+2)
-    return h+2,w+2,mp,found
-def TI(n):return GI(n,n-1)
-def accum(ls):
-    rt=[0]
-    for i in ls:rt+=[rt[-1]+i]
-    return rt
-def bit_combination(n,base=2):
-    rt=[]
-    for tb in range(base**n):s=[tb//(base**bt)%base for bt in range(n)];rt+=[s]
-    return rt
-def gcd(x,y):
-    if y==0:return x
-    if x%y==0:return y
-    while x%y!=0:x,y=y,x%y
-    return y
-def YN(x):print(['NO','YES'][x])
-def Yn(x):print(['No','Yes'][x])
-def show(*inp,end='\n'):
-    if show_flg:print(*inp,end=end)
-
-mo=10**9+7
-inf=float('inf')
-FourNb=[(-1,0),(1,0),(0,1),(0,-1)];EightNb=[(-1,0),(1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)];compas=dict(zip('WENS',FourNb));cursol=dict(zip('LRUD',FourNb))
-l_alp=string.ascii_lowercase
-#sys.setrecursionlimit(10**7)
-read=sys.stdin.buffer.read
-readline=sys.stdin.buffer.readline
-input=lambda: sys.stdin.readline().rstrip()
-
 class Tree:
     def __init__(self,inp_size=None,ls=None,init=True,index=1):
         self.LCA_init_stat=False
@@ -240,16 +164,16 @@ class Tree:
         else:
             print(self)
  
-    def dfs2(self,x,func=lambda pr,prv,nx,dist:prv+dist,root_v=0):
+    def dfs_tour(self,x,root_dep=0):
         q=deque([x])
-        v=[None]*self.node_size
-        v[x]=root_v
+        dep=[None]*self.node_size
+        dep[x]=root_dep
         dfs_tr=[x]
         while q:
             c=q.pop()
             for nb,d in self.edges[c]:
-                if v[nb]==None:
+                if dep[nb]==None:
                     q.append(nb)
-                    v[nb]=func(c,v[c],nb,d)
+                    dep[nb]=dep[c]+1
                     dfs_tr+=nb,
-        return v,dfs_tr
+        return dep,dfs_tr
