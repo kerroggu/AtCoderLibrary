e286e539e93383137680bc4c2518ab3e4d7677d8
keroru <49674292+kerroggu@users.noreply.github.com>
2021-08-25 13:17:54 +0900

    Changed to store MOD as member variable

--- src/Comb.py
+++ src/Comb.py
@@ -1,18 +1,20 @@
-
+## Verified by ABC 215-G
+## https://atcoder.jp/contests/abc215/tasks/abc215_g
 
 class Comb:
     def __init__(self,n,mo=10**9+7):
+        self.mo=mo
         self.fac=[0]*(n+1)
         self.inv=[1]*(n+1)
         self.fac[0]=1
         self.fact(n)
         for i in range(1,n+1):
-            self.fac[i]=i*self.fac[i-1]%mo
+            self.fac[i]=i*self.fac[i-1]%self.mo
             self.inv[n]*=i
-            self.inv[n]%=mo
-        self.inv[n]=pow(self.inv[n],mo-2,mo)
+            self.inv[n]%=self.mo
+        self.inv[n]=pow(self.inv[n],self.mo-2,self.mo)
         for i in range(1,n):
-            self.inv[n-i]=self.inv[n-i+1]*(n-i+1)%mo
+            self.inv[n-i]=self.inv[n-i+1]*(n-i+1)%self.mo
         return
     
     def fact(self,n):
@@ -24,13 +26,17 @@ class Comb:
     def comb(self,x,y):
         if y<0 or y>x:
             return 0
-        return self.fac[x]*self.inv[x-y]*self.inv[y]%mo
+        return self.fac[x]*self.inv[x-y]*self.inv[y]%self.mo
+
+    def rcomb(self,x,y):
+        if y<0 or y>x:
+            return 0
+        return self.inv[x]*self.fac[x-y]*self.fac[y]%self.mo
 
     def cat(self,x):
         if x<0:
             return 0
-        return self.fac[2*x]*self.inv[x]*self.inv[x+1]%mo
-
+        return self.fac[2*x]*self.inv[x]*self.inv[x+1]%self.mo
 
 def Golf_Comb(x,y,n=10**5,M=10**9+7):
     M=10**9+7
