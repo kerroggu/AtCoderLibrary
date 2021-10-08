0d68edff76dadf6b9e8d7979b89e157a37d7335c
keroru <49674292+kerroggu@users.noreply.github.com>
2021-10-08 17:11:29 +0900

    1st creation

---
+++ src/minCostFlow.py
@@ -0,0 +1,56 @@
+## From https://tjkendev.github.io/procon-library/python/min_cost_flow/primal-dual.html
+## Verified by PAST_8 K
+## https://atcoder.jp/contests/past202109-open/tasks/past202109_k
+
+class MinCostFlow:
+    def __init__(self, n):
+        self.n = n
+        self.G = [[] for i in range(n)]
+
+    def addEdge(self, f, t, cap, cost):
+        # [to, cap, cost, rev]
+        self.G[f].append([t, cap, cost, len(self.G[t])])
+        self.G[t].append([f, 0, -cost, len(self.G[f])-1])
+
+    def minCostFlow(self, s, t, f):
+        rt=0
+        n = self.n
+        G = self.G
+        prevv = [0]*n; preve = [0]*n
+        INF = 10**9+7
+
+        res = 0
+        while f:
+            dist = [INF]*n
+            dist[s] = 0
+            update = 1
+            while update:
+                update = 0
+                for v in range(n):
+                    if dist[v] == INF:
+                        continue
+                    gv = G[v]
+                    for i in range(len(gv)):
+                        to, cap, cost, rev = gv[i]
+                        if cap > 0 and dist[v] + cost < dist[to]:
+                            dist[to] = dist[v] + cost
+                            prevv[to] = v; preve[to] = i
+                            update = 1
+            if dist[t] == INF:
+                rt=min(rt,res)
+                return rt
+
+            d = f; v = t
+            while v != s:
+                d = min(d, G[prevv[v]][preve[v]][1])
+                v = prevv[v]
+            f -= d
+            res += d * dist[t]
+            v = t
+            while v != s:
+                e = G[prevv[v]][preve[v]]
+                e[1] -= d
+                G[v][e[3]][1] += d
+                v = prevv[v]
+            rt=min(rt,res)
+        return rt
