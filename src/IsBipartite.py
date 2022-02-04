ff69d26e0ed85a366bfe80dcbf9f021171b10467
keroru <49674292+kerroggu@users.noreply.github.com>
2022-02-04 19:39:06 +0900

    Bug fix

--- src/IsBipartite.py
+++ src/IsBipartite.py
@@ -1,16 +1,21 @@
+# Verified by
+# https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_ao
 # edgeを引数に二部グラフ判定
 
-v=[-1 for i in range(n)]
-
 def IsBipartite(edge):
-    q=[0]
-    v[0]=0
-    while q:
-        c=q.pop()
-        for n in edge[c]:
-            if v[n]==-1:
-                q.append(n)
-                v[n]=1-v[c]
-            elif v[n]==v[c]:
-                return False
+    n=len(edge)
+    v=[-1 for i in range(n)]
+    for c in range(n):
+        if v[c]!=-1:
+            continue
+        q=[c]
+        v[c]=0
+        while q:
+            c=q.pop()
+            for nb in edge[c]:
+                if v[nb]==-1:
+                    q.append(nb)
+                    v[nb]=1-v[c]
+                elif v[nb]==v[c]:
+                    return False
     return True
