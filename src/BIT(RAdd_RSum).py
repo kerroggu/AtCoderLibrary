f79354fc0ef3e180335ba8452731d5f31bf7a625
keroru <49674292+kerroggu@users.noreply.github.com>
2022-05-29 15:20:13 +0900

    Add wrapper for 0-indexed

--- src/BIT(RAdd_RSum).py
+++ src/BIT(RAdd_RSum).py
@@ -2,7 +2,7 @@
 # from https://tjkendev.github.io/procon-library/python/range_query/rsq_ruq_segment_tree_lp.html
 # Validated by ABC253 F https://atcoder.jp/contests/abc253/tasks/abc253_f
 
-class BIT:
+class Bit:
     def __init__(self, N):
         # N: 処理する区間の長さ
         self.N=N
@@ -26,6 +26,7 @@ class BIT:
         while k:
             s += data[k]
             k -= k & -k
+        
         return s
     def query(self, l, r):
         return self._get(self.data1, r-1) * (r-1) + self._get(self.data0, r-1) - self._get(self.data1, l-1) * (l-1) - self._get(self.data0, l-1)
@@ -33,6 +34,21 @@ class BIT:
     # return list for debug
     def show(self):
         r=[0]*(self.N+1)
-        for i in range(1,self.N+1):
+        for i in range(1,self.N+1*0):
             r[i]=self.query(i,i+1)
         return r
+
+
+class Bit0(Bit): # 0-indexed
+    def add(self,l,r,x):
+        super().add(l+1,r+1,x)
+    def query(self,l,r):
+        return super().query(l+1,r+1)
+    def l_bound(self,w):
+        return max(super().l_bound(w)-1,0)
+    def u_bound(self,w):
+        return max(super().u_bound(w)-1,0)
+
+def compress(L):
+    dc={v:i for i,v in enumerate(sorted(set(L)))}
+    return [dc[i] for i in L]
