9012de262fe841a0dccaa859e9d67b16e2166f06
keroru <49674292+kerroggu@users.noreply.github.com>
2022-09-17 23:05:48 +0900

    Added 2nd pattern

--- src/_Template_Interacitve.py
+++ src/_Template_Interacitve.py
@@ -27,3 +27,45 @@ def solve(n,mode='Sub'):
         else: # for Test
             y=judge(x)
         return y[0]
+
+## verified by ABC269-E
+## https://atcoder.jp/contests/abc269/tasks/abc269_e    
+    def solve(n,mode='Sub'):
+    if mode!='Sub': # set a test case in the following block
+        R=5
+        C=2
+        
+    def judge(a,b,c,d):
+        if (c,d)==(1,n):
+            rt=b-a+1 - (a<=R<=b)
+        else:
+            rt=d-c+1 - (c<=C<=d)
+        return rt
+ 
+    def query(a,b,c,d):
+        x=[a,b,c,d]
+        print('?',*[a,b,c,d],flush=True)
+        sys.stdout.flush()
+        
+        if mode=='Sub': # for Submission
+            y=I()
+        else: # for Test
+            y=judge(*x)
+        return y
+    
+    D=10
+    l,r=1,n
+    for i in range(D):
+        m=l+r>>1
+        y=query(l,m,1,n)
+        if m-l+1!=y:
+            r=m
+        else:
+            l=m+1
+    ans=[l]
+    
+    return ans
+
+n=I()
+ans=solve(n)
+print('!',*ans,flush=True)
