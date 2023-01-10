16e6ee0b3a2ebd9ff180e842284b15cdbbc49d30
keroru <49674292+kerroggu@users.noreply.github.com>
2023-01-11 00:08:52 +0900

    Classified

--- src/Rolling_hash.py
+++ src/Rolling_hash.py
@@ -1,18 +1,46 @@
-def rolling_hash(a,m,mo=10**9+7): # return list of rolling_hash values of [a[i:i+m] for i in range(n-m+1)]
-    n=len(a)
-    z=pow(2,101,mo)
-    pz=[]
-    zm=1
-    for i in range(m):
-        pz+=zm,
-        zm=zm*z%mo
-    pz=pz[::-1]
-    ha=0
-    for i in range(m):
-        ha=(ha+a[i]*pz[i])%mo
+# veryfied by ABC284F
+# https://atcoder.jp/contests/abc284/tasks/abc284_f
+
+# sample
+#
+# mo=10**9+7
+# b=RI(2,mo-1)
+# rh=RollingHash(a,b,mo)
+# rh2=RollingHash(a[::-1],rh.base,rh.mod)
+
+class RollingHash():
+    def __init__(self,s,base=None,mod=10**9+7):
+        self.n=len(s)
+        self.mod=mod
+        if base==None:
+            import random
+            base=random.randint(0,mod-1)
+        self.base=base
+        self.pw=[1]*(self.n+1)
+        self.rp=[1]*(self.n+1)
+        self.hash=[0]*(self.n+1)
+        
+        B=pow(self.base,self.n-1,self.mod)
+        rb=pow(self.base,self.mod-2,self.mod)
+        self.rp[1]=rb
+        self.hash[1]=s[0]*B
+        for i in range(1,self.n):
+            B=(B*rb)%self.mod
+            self.hash[i+1]=(self.hash[i]+s[i]*B)%self.mod
+            self.pw[i+1]=(self.pw[i]*self.base)%self.mod
+            self.rp[i+1]=(self.rp[i]*rb)%self.mod
     
-    rt=[ha]
-    for i in range(n-m):
-        ha=(ha*z-a[i]*zm+a[i+m])%mo
-        rt+=ha,
-    return rt
+    def get(self,*args): # 引数が一つで m の時、長さmの部分列のローリングハッシュを返す。引数が2つで l,r の時、[l, r) のハッシュを返す (rは開区間)
+        if len(args)==1:
+            m=args[0]
+            return [(self.hash[l+m] - self.hash[l])*self.pw[m]%self.mod for l in range(len(self.hash)-m)]
+        else:
+            l,r=args
+            return (self.hash[r] - self.hash[l])*self.rp[self.n-r]%self.mod
+
+    def concat(self,l1,r1,l2,r2):
+        return (self.get(l1,r1)*self.pw[r2-l2+1]+self.get(l2,r2) )%self.mod
+
+    def __str__(self):
+        return str(self.hash)
+    
