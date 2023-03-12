9f71696a528c632732317d6cb1a2d9bfd6e65a75
keroru <49674292+kerroggu@users.noreply.github.com>
2023-03-13 02:06:13 +0900

    Speed up

--- src/PrimeFuncTools.py
+++ src/PrimeFuncTools.py
@@ -5,30 +5,87 @@
 
 ## return prime factors of N as dictionary {prime p:power of p}
 ## within 2 sec for N = 2*10**20+7
-def primeFactor(N):
-    i,n=2,N
-    ret={}
-    d,sq=2,99
-    while i<=sq:
-        k=0
-        while n%i==0:
-            n,k,ret[i]=n//i,k+1,k+1
-        if k>0 or i==97:
-            sq=int(n**(1/2)+0.5)
-        if i<4:
-            i=i*2-1
-        else:
-            i,d=i+d,d^6
-    if n>1:
-        ret[n]=1
+def isPrimeMR(n):
+    d = n - 1
+    d = d // (d & -d)
+    L = [2, 7, 61] if n < 1<<32 else [2, 3, 5, 7, 11, 13, 17] if n < 1<<48 else [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+    for a in L:
+        t = d
+        y = pow(a, t, n)
+        if y == 1: continue
+        while y != n - 1:
+            y = y * y % n
+            if y == 1 or t == n - 1: return 0
+            t <<= 1
+    return 1
+    
+def findFactorRho(n):
+    m = 1 << n.bit_length() // 8
+    for c in range(1, 99):
+        f = lambda x: (x * x + c) % n
+        y, r, q, g = 2, 1, 1, 1
+        while g == 1:
+            x = y
+            for i in range(r):
+                y = f(y)
+            k = 0
+            while k < r and g == 1:
+                ys = y
+                for i in range(min(m, r - k)):
+                    y = f(y)
+                    q = q * abs(x - y) % n
+                g = gcd(q, n)
+                k += m
+            r <<= 1
+        if g == n:
+            g = 1
+            while g == 1:
+                ys = f(ys)
+                g = gcd(abs(x - ys), n)
+        if g < n:
+            if isPrimeMR(g): return g
+            elif isPrimeMR(n // g): return n // g
+            return findFactorRho(g)
+            
+def primeFactor(n):
+    i = 2
+    ret = {}
+    rhoFlg = 0
+    while i * i <= n:
+        k = 0
+        while n % i == 0:
+            n //= i
+            k += 1
+        if k: ret[i] = k
+        i += i % 2 + (3 if i % 3 == 1 else 1)
+        if i == 101 and n >= 2 ** 20:
+            while n > 1:
+                if isPrimeMR(n):
+                    ret[n], n = 1, 1
+                else:
+                    rhoFlg = 1
+                    j = findFactorRho(n)
+                    k = 0
+                    while n % j == 0:
+                        n //= j
+                        k += 1
+                    ret[j] = k
+    
+    if n > 1: ret[n] = 1
+    if rhoFlg: ret = {x: ret[x] for x in sorted(ret)}
     return ret
 
 ## return divisors of n as list
-def divisors(n):
-    div=[1]
-    for i,j in primeFactor(n).items():
-        div=[(i**k)*d for d in div for k in range(j+1)]
-    return div
+def divisors(N):
+    pf = primeFactor(N)
+    ret = [1]
+    for p in pf:
+        ret_prev = ret
+        ret = []
+        for i in range(pf[p]+1):
+            for r in ret_prev:
+                ret.append(r * (p ** i))
+    return sorted(ret)
 
 ## return the array s such that s[q] = the minimum prime factor of q
 def sieve(x):
