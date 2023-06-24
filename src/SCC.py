6768edadf98b59d14a37f6e3047b9ae5af2a3db7
keroru <49674292+kerroggu@users.noreply.github.com>
2023-06-24 13:39:18 +0900

    Create SCC.py
    Initial version

---
+++ src/SCC.py
@@ -0,0 +1,69 @@
+## Original https://github.com/shakayami/ACL-for-python/blob/master/scc.py
+## verified by Typical_90_no_21 https://atcoder.jp/contests/typical90/tasks/typical90_u
+
+def scc(my_edges):
+    edges=[]
+    N=len(my_edges)
+    for u in range(N):
+        for v,d in my_edges[u]:
+            edges+=(u,v),
+    M=len(edges)
+    start=[0]*(N+1)
+    elist=[0]*M
+    for e in edges:
+        #show(e)
+        start[e[0]+1]+=1
+    for i in range(1,N+1):
+        start[i]+=start[i-1]
+    counter=start[:]
+    for e in edges:
+        elist[counter[e[0]]]=e[1]
+        counter[e[0]]+=1
+    visited=[]
+    low=[0]*N
+    Ord=[-1]*N
+    ids=[0]*N
+    NG=[0,0]
+    def dfs(v):
+        stack=[(v,-1,0),(v,-1,1)]
+        while stack:
+            v,bef,t=stack.pop()
+            if t:
+                if bef!=-1 and Ord[v]!=-1:
+                    low[bef]=min(low[bef],Ord[v])
+                    stack.pop()
+                    continue
+                low[v]=NG[0]
+                Ord[v]=NG[0]
+                NG[0]+=1
+                visited.append(v)
+                for i in range(start[v],start[v+1]):
+                    to=elist[i]
+                    if Ord[to]==-1:
+                        stack.append((to,v,0))
+                        stack.append((to,v,1))
+                    else:
+                        low[v]=min(low[v],Ord[to])
+            else:
+                if low[v]==Ord[v]:
+                    while(True):
+                        u=visited.pop()
+                        Ord[u]=N
+                        ids[u]=NG[1]
+                        if u==v:
+                            break
+                    NG[1]+=1
+                low[bef]=min(low[bef],low[v])
+    for i in range(N):
+        if Ord[i]==-1:
+            dfs(i)
+    for i in range(N):
+        ids[i]=NG[1]-1-ids[i]
+    group_num=NG[1]
+    counts=[0]*group_num
+    for x in ids:
+        counts[x]+=1
+    groups=[[] for i in range(group_num)]
+    for i in range(N):
+        groups[ids[i]].append(i)
+    return groups
