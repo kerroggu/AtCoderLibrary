5110d0cc521ab2f2ac4c7b582bdfa9f6ce14f071
keroru <49674292+kerroggu@users.noreply.github.com>
2023-11-15 10:27:10 +0900

    Update _Template_ShortCode.py

--- src/_Template_ShortCode.py
+++ src/_Template_ShortCode.py
@@ -1,3 +1,15 @@
-# input
-n,*a=map(int,open(0).read().split())# n\n a1 a2 a3\n -> n=n,a=[a1,a2,a3]
-*a,=map(int,open(0))# x\n y\n z\n -> a=[x,y,z]
+################## input ##################
+
+###  n\n a1 a2 a3\n -> n=n,a=[a1,a2,a3]
+n,*a=map(int,open(0).read().split())
+
+###  x\n y\n z\n -> a=[x,y,z]
+*a,=map(int,open(0))
+
+
+################## combination ##################
+
+###  (n,k)
+w=1
+for i in range(k):w=w*(n-i)*pow(i+1,-1,M)%M
+print(w)
