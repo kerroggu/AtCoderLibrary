54e1850b4ab12e96d46ebadd23b405f94d65441f
keroru <49674292+kerroggu@users.noreply.github.com>
2023-11-20 18:59:01 +0900

    Update _Template_ShortCode.py

--- src/_Template_ShortCode.py
+++ src/_Template_ShortCode.py
@@ -3,6 +3,9 @@
 ###  n\n a1 a2 a3\n -> n=n,a=[a1,a2,a3]
 n,*a=map(int,open(0).read().split())
 
+###  n\n a1 a2 a3\n -> a=[a1,a2,a3] # No need 'n'
+a=map(int,[*open(0)][1].split())
+
 ###  x\n y\n z\n -> a=[x,y,z]
 *a,=map(int,open(0))
 
@@ -13,3 +16,18 @@ n,*a=map(int,open(0).read().split())
 w=1
 for i in range(k):w=w*(n-i)*pow(i+1,-1,M)%M
 print(w)
+
+################## bfs ##################
+while Q:
+ N=[]
+ for c in Q:
+  for z in g[c]:
+   if D[z]==n:N+=z,;D[z]=D[c]+1;p[z]=c
+ Q=N
+
+################## binary search ##################
+l,r=0,99**9
+while~l+r:
+ m=l+r>>1
+ if c(m):r=m
+ else:l=m
